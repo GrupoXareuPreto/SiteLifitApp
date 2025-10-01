3ee429c138b6f637a4191044c6da3461b2b1ba91
Miguel <xareupreto@gmail.com>
2025-10-01 15:58:42 -0300

    Ícone do site alterado. Hero image finalizada. Iniciando FeatureSection

--- SiteLifit/src/app/app.component.ts
+++ SiteLifit/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FeatureSectionComponent } from './feature-section/feature-section.component';
 import { HeaderComponent } from './header/header.component';
 import { HeroComponent } from "./hero/hero.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, HeroComponent],
+  imports: [RouterOutlet, HeaderComponent, HeroComponent, FeatureSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
