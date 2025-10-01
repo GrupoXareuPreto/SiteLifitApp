37753a2411d8ee967def0e40a215f275f36e64d8
Miguel-637 <miguelpifanif@gmail.com>
2025-09-30 23:01:35 -0300

    Trabalhando na estilização da navbar e na hero image

---
+++ SiteLifit/src/app/hero/hero.component.ts
@@ -0,0 +1,13 @@
+import { Component, HostListener } from '@angular/core';
+import {trigger,style,transition,animate} from '@angular/animations';
+
+@Component({
+  selector: 'app-hero',
+  imports: [],
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css',
+ 
+})
+export class HeroComponent {
+ 
+}
