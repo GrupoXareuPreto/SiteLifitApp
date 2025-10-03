ce48e8e59d9ba61146d80f87f0916493a95a8310
Arthur <bergamaschiarthur6@gmai.com>
2025-10-03 17:45:29 -0300

    Criacao do componente metas

--- SiteLifit/src/app/app.component.ts
+++ SiteLifit/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { FeatureSectionComponent } from './feature-section/feature-section.component';
 import { HeaderComponent } from './header/header.component';
 import { HeroComponent } from "./hero/hero.component";
+import { MetasComponent } from "./metas/metas.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, HeroComponent, FeatureSectionComponent],
+  imports: [RouterOutlet, HeaderComponent, HeroComponent, FeatureSectionComponent, MetasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
