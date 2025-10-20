92f9a1ec2ce9a8faf3d137d7a6e4409466365585
unknown <miguelpifani@gmail.com>
2025-10-20 14:47:55 -0300

    teste accordion

--- SiteLifit/src/app/pages/faq/faq.component.ts
+++ SiteLifit/src/app/pages/faq/faq.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { FooterComponent } from '../../shared/footer/footer.component';
+import { AccordionComponent } from '../../accordion/accordion.component';
+
 
 @Component({
   selector: 'app-faq',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, AccordionComponent],
   templateUrl: './faq.component.html',
   styleUrl: './faq.component.css'
 })
