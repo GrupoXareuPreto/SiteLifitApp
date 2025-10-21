2a7a4bb35faa2a9d53039e9b5c58411b6487b5c5
RogerioZFaga <rogeriozfaga@gmail.com>
2025-10-21 17:15:07 -0300

    corrigido animação hero

--- SiteLifit/src/app/pages/home/components/hero/hero.component.ts
+++ SiteLifit/src/app/pages/home/components/hero/hero.component.ts
@@ -5,16 +5,25 @@ import { HeaderComponent } from '../../../../shared/header/header.component';
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.css',
+  styleUrls: ['./hero.component.css'],
 
 
 })
 export class HeroComponent implements OnInit {
-  isVisible = false;
-  mensagem = ''
+   isVisible = false;
+  mensagem = 'Inicie sua jornada com a gente';
 
-  ngOnInit(): void {
-    this.isVisible = true
-    this.mensagem = 'Seu Impulso para uma vida melhor'
+  ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.startHeroAnimation();
+  }
+
+  startHeroAnimation(delay = 40) {
+    this.isVisible = false;
+    setTimeout(() => {
+      this.isVisible = true;
+    }, delay);
   }
 }
